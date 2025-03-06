843556825d4510156d82fa522b05bda5681f6c78
Leonardo Lessa <leolopes00@hotmail.com>
2025-03-06 16:49:34 -0300

    fatura btg funcionando

--- src/extrator_btg.py
+++ src/extrator_btg.py
@@ -1,9 +1,9 @@
 import pypdf
-from src.lib.boilerplate import current, find_monetary, find_date, is_record
+import re
+from src.lib.boilerplate import current, is_record
 
 
 def process_pdf_btg(reader: pypdf.PdfReader) -> list[list[str]]:
-    # TODO: Nao funciona ainda
     bad_records = []
     register = None
     registers = []
@@ -13,16 +13,22 @@ def process_pdf_btg(reader: pypdf.PdfReader) -> list[list[str]]:
         index = 0
         for index in range(len(tokens)):
             actual = current(tokens, index)
-            date = find_date(actual)
-            money = find_monetary(actual)
-            print(actual)
+            next_token = current(tokens, index+1)
+            record = find_register_btg(actual)
+            record_date = find_register_date_btg(next_token)
+            if record_date is None or record is None:
+                continue
 
-            if date and money and is_record(actual, bad_records):
-                date_str = date.group()
-                money_str = money.group()
+            monthStr = record_date.group()[-3:]
+            record_month = month_name_to_number(monthStr)
+
+            if is_record(actual, bad_records) and record_month:
+                date_str = record_date.group().replace(" "+monthStr, "/"+record_month)
+                money_str = find_monetary_btg(record.group()).group()
 
                 description = actual.replace(
-                    date_str, "").replace(money_str, "").strip()
+                    date_str, "").replace(money_str, "")
+                description = description.replace("R$", "").strip()
 
                 register = [date_str]
                 register.append(description)
@@ -32,3 +38,34 @@ def process_pdf_btg(reader: pypdf.PdfReader) -> list[list[str]]:
                 continue
 
     return registers
+
+
+def find_register_btg(text: str):
+    return re.search(r"(R|US)\$\s?\d+(?:,\d{2})?\w+(.*)", text)
+
+
+def find_register_date_btg(text: str):
+    return re.search(r"\d{2} \w+", text)
+
+
+def find_monetary_btg(text: str):
+    return re.search(r"\b\d+(?:,\d+)", text)
+
+
+def month_name_to_number(month: str) -> int:
+    meses = {
+        'jan': '01',
+        'fev': '2',
+        'mar': '3',
+        'abr': '4',
+        'mai': '5',
+        'jun': '6',
+        'jul': '7',
+        'ago': '8',
+        'set': '9',
+        'out': '10',
+        'nov': '11',
+        'dez': '12'
+    }
+
+    return meses.get(month.lower(), "")
--- src/lib/boilerplate.py
+++ src/lib/boilerplate.py
@@ -2,16 +2,20 @@ import re
 
 
 def current(tokens: list[str], index: int, offset=0):
-    return tokens[index+offset] if index+offset < len(tokens) else None
+    # Retorna o token atual dentro de um offset
+    return tokens[index+offset] if index+offset < len(tokens) else ""
 
 
 def find_date(text: str):
+    # Retorna se existe uma data no formato DD/MM sem /YYYY
     return re.search(r"\b\d{2}/\d{2}(?!/\d{4})\b", text)
 
 
 def find_monetary(text: str):
+    # Retorna se existe um numero no formato 00,00
     return re.search(r"\b\d+(?:,\d+)+\b", text)
 
 
 def is_record(text: str, bad_records: str) -> bool:
+    # Verifica se o text existe dentro dos bad_records
     return not any(bad_record in text for bad_record in bad_records)
