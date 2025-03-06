0e4d73c603fa22f4741d3cfeb7649ddd9abc8af6
Leonardo Lessa <leolopes00@hotmail.com>
2025-03-06 14:16:50 -0300

    using new library and remaking the program

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import PyPDF2
+import pypdf
 import re
 import logging
 import sys
@@ -7,105 +7,74 @@ import sys
 logging.getLogger().setLevel(logging.DEBUG)
 
 
-tokens = None
-registers = []
-index = 0
-doc_name = None
+def main():
+    doc = sys.argv[1]
+    doc_name = doc.split('/')[-1]
+    reader = pypdf.PdfReader(doc)
 
-def next():
-    global index
-    index += 1
-    return current()
-
-
-def next_document():
-    global doc_name
-
-    docs = sys.argv[1:]
-    for doc in docs:
-        doc_name = doc.split('/')[-1]
-        reader = PyPDF2.PdfReader(doc)
-        logging.info(f'Number of pages in "{doc}": ' + str(len(reader.pages)))
-        yield reader
+    logging.info(f'Number of pages in "{doc}": ' + str(len(reader.pages)))
+    registers = process_pdf(reader, doc_name)
+    output_excel(registers)
 
 
-def next_page():
-    global index
-    global tokens
+def process_pdf(reader: pypdf.PdfReader, doc_name: str):
+    # TODO: Diferenciar compras dessa fatura e da próxima
+    register = None
+    registers = []
 
-    for reader in next_document():
-        for page in reader.pages:
-            tokens = page.extract_text().split('\n')
-            index = 0
-            yield True
+    for page in reader.pages:
+        tokens = page.extract_text().split('\n')
+        index = 0
+        for index in range(len(tokens)):
+            actual = current(tokens, index)
+            date = find_date(actual)
+            money = find_monetary(actual)
 
-def current(offset=0):
+            if date and money and is_record(actual):
+                date_str = date.group()
+                money_str = money.group()
+
+                description = actual.replace(
+                    date_str, "").replace(money_str, "").strip()
+
+                register = [date_str]
+                register.append(description)
+                register.append(money_str)
+                registers.append(register)
+                register = None
+                continue
+
+    return registers
+
+
+def current(tokens: list[str], index: int, offset=0):
     return tokens[index+offset] if index+offset < len(tokens) else None
 
 
-def find_monetary(text):
-    return re.search("[0-9]+,[0-9]{2}", text)
+def find_date(text: str):
+    return re.search(r"\b\d{2}/\d{2}(?!/\d{4})\b", text)
 
 
-def find_date(text):
-    return re.search("^[0-9]{2}/[0-9]{2}$", text)    
+def find_monetary(text: str):
+    return re.search(r"\b\d+(?:,\d+)+\b", text)
 
-register = None
-is_correct_table = False
 
-for page in next_page():
-    while next():
-        logging.debug(current())
+def is_record(text: str) -> bool:
+    bad_records: list[str] = ["Pagamento efetuado"]
+    for i in bad_records:
+        if text.find(i) != -1:
+            return False
+    return True
 
-        if current().lower() in ["lançamentos: compras e saques", "lançamentos: produtos e serviços"]:
-            is_correct_table = True
-            continue
 
-        if current().lower() in ["compras parceladas - próximas faturas"]:
-            is_correct_table = False
-            continue
+def output_excel(registers: list[list[str]]):
+    sep = ';'
+    with open('ouput.csv', 'w') as f:
+        data = sep.join(['date', 'description', 'value']) + '\n'
+        for register in registers:
+            data += sep.join(register) + ' \n'
+        f.write(data)
 
-        if find_date(current()) and is_correct_table:
-            logging.debug('starting register')
-            register = [doc_name, current()]
-            continue
 
-        if find_monetary(current()) and register:
-            logging.debug('closing register')
-            value = float(current().replace(',', '.').replace(' ', ''))
-            
-            category = ""
-            for i in range(1,7):
-                offset = current(offset=i)
-                logging.debug(f'category attemp {i}: {offset}')
-                if offset and '.' in offset and ',' not in offset:
-                    category = offset
-                    break
-            
-            category_name = category.split('.')[0].strip()
-            city = '.'.join(category.split('.')[1:]).strip()
-
-            register += [category_name, city]
-            register.append(value)            
-            registers.append(register)            
-            register = None
-            continue
-
-        if register:
-            logging.debug('complementing register')
-            _register = current()
-            _parc = re.search("[0-9]{2}/[0-9]{2}", _register)
-            _parc, _total = _parc.group().split('/') if _parc else ["", ""]
-            register += [_register, _parc, _total]     
-            continue
-  
-sep = ';'
-with open('ouput.csv', 'w') as f:
-    t = [[register if isinstance(register, str) else str(register).replace('.',',') for register in row] for row in registers]
-    data = sep.join(['filename','date','description','parc','total_parc','category','city','value']) + '\n'
-    data += '\n'.join([ sep.join(register) for register in t])
-    f.write(data)
-
-logging.debug(registers)
-logging.info('total of registers ' + str(len(registers)))
-logging.info('total of money ' + str(sum([r[-1] for r in registers])))
+if __name__ == "__main__":
+    main()
